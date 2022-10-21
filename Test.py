c2d8dd7d528efedf28b8115c0f15d2d2457b4174
pmcginn <patrick@mcginn.family>
2022-10-21 11:57:53 -0700

    Day 3

--- Test.py
+++
@@ -1,6 +0,0 @@
-age = int(input("How old are you? "))
-weeks = age * 52
-days = age * 365
-months = age * 12
-age_at_death = 90
-print(f"You will die in {(age_at_death * 365) - days} days, {(age_at_death * 52) - weeks} weeks, and {(age_at_death * 12) - months} months.")
